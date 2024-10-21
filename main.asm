e8975dadb040957f216a1e9711efd70472008611
Lazlo Steele <lazlosteele@gmail.com>
2024-10-20 18:11:57 -0600

    mode saved to mode_flag and state machine is implemented

--- main.asm
+++ main.asm
@@ -1,8 +1,8 @@
 # Program #8: Number Base Converter
 # Author: Lazlo F. Steele
-# Due Date : Oct. 19, 2024 Course: CSC2025-2H1
-# Created: Oct. 14, 2024
-# Last Modified: Oct. 14, 2024
+# Due Date : Oct. 26, 2024 Course: CSC2025-2H1
+# Created: Oct. 20, 2024
+# Last Modified: Oct. 20, 2024
 # Functional Description: Given a 32-bit numeric value provided by the user, convert from base 2/10/16 to base 2/10/16.
 # Language/Architecture: MIPS 32 Assembly
 ####################################################################################################
@@ -14,29 +14,73 @@
 .include	"messages.asm"
 
 .data
-			.align	2
-	buffer:	.space	32
+	mode_flag:	.byte	0		# 0 - off, 1 - integer, 2 - binary, 3 - hex
+				.align	2
+	buffer:		.space	32
 
 .globl	main
 
 .text
+####################################################################################################
+# function: main
+# purpose: to control program flow
+# registers used:
+#	$a0 - passing arugments to subroutines
+####################################################################################################
 main:
-	call (welcome)
-	call (prompt)
+	call(welcome)
+	call(get_mode)
+	call(get_number)
 	
 	j again_loop
 	
 welcome:
-	print_str (welcome_msg)
+	print_str(welcome_msg)
 	jr $ra
-prompt:
-	print_str (mode_msg)
-	print_str (mode_prmpt)
 	
-	read_str(buffer, 32)
+get_mode:
+	print_str(mode_msg)
+	print_str(mode_prmpt)
 	
-	jr $ra
+	mode_input:
+		read_str(buffer, 1)
+	
+	la	$t0, buffer
+	lw	$t1, 0($t0)
+	blt $t1, 49, invalid_mode
+	bgt $t1, 51, invalid_mode
+	
+	addi $t1, $t1, -48
+	
+	la	$t2, mode_flag
+	sb	$t1, 0($t2)
 
+	jr $ra
+	
+	invalid_mode:
+		print_str(invalid_msg)
+		j get_mode
+
+get_number:
+	lb $t0, mode_flag
+	
+	beq $t0, 1, get_decimal
+	beq $t0, 2, get_binary
+	beq $t0, 3, get_hex
+
+	get_decimal:
+		print_str(decimal_msg)
+		read_str(buffer, 10)
+		jr $ra
+	get_binary:
+		print_str(binary_msg)
+		read_str(buffer, 32)
+		jr $ra
+	get_hex:
+		print_str(hex_msg)
+		read_str(buffer, 8)
+		jr $ra
+		
 again_loop:
 	again
 	j again_loop
